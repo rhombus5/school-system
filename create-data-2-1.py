380cd247d5fa975bd09d9e7d3daee3ea9fd9190e
Joseph Hughes <159965229+rhombus5@users.noreply.github.com>
2024-10-15 10:20:16 -0400

    Update create-data-2-1.py

--- create-data-2-1.py
+++ create-data-2-1.py
@@ -17,60 +17,64 @@ connection = pymysql.connect(
     database=database
 )
 
-conn = connection.cursor()
+cursor = connection.cursor()
 
 alldict = {}
 
-sql1 = ("TRUNCATE table jph_users;")
-conn.execute(sql1)
-sql2 = ("TRUNCATE table jph_sections;")
-conn.execute(sql2)
-sql3 = ("TRUNCATE table jph_roles;")
-conn.execute(sql3)
-sql4 = ("TRUNCATE table jph_enrollments;")
-conn.execute(sql4)
-sql5 = ("TRUNCATE table jph_finalgrades;")
-conn.execute(sql5)
+truncate_sqls = [
+    "TRUNCATE table jph_users;",
+    "TRUNCATE table jph_sections;",
+    "TRUNCATE table jph_roles;",
+    "TRUNCATE table jph_enrollments;",
+    "TRUNCATE table jph_finalgrades;"
+]
+
+for sql in truncate_sqls:
+    cursor.execute(sql)
 
 user_sql = "INSERT INTO jph_users (FirstName, LastName, Phone) VALUES "
-for x in range (0, 500):
-    user_sql += f"({fake.first_name()}, {fake.last_name()}, {fake.phone_number()}) "
+for x in range(500):
+    user_sql += f"('{fake.first_name()}', '{fake.last_name()}', '{fake.phone_number()}'), "
+user_sql = user_sql.rstrip(', ') + ';'
 print(user_sql)
-conn.execute(user_sql)
+cursor.execute(user_sql)
 
 section_sql = "INSERT INTO jph_sections (ClassID, SectionNumber, YearTermID) VALUES "
 for ytID in range(1, 4):
     for sN in range(1, 51):
         cID = random.randrange(1, 20)
-        section_sql += f"({cID}, {sN}, {ytID}) "
+        section_sql += f"({cID}, {sN}, {ytID}), "
+section_sql = section_sql.rstrip(', ') + ';'
 print(section_sql)
-conn.execute(section_sql)
+cursor.execute(section_sql)
 
-role_sql = "INSERT INTO jph_roles (RoleName) VALUES ('Teacher'), ('Student')"
+role_sql = "INSERT INTO jph_roles (RoleName) VALUES ('Teacher'), ('Student');"
 print(role_sql)
-conn.execute(role_sql)
+cursor.execute(role_sql)
 
 enrollment_sql = "INSERT INTO jph_enrollments (UserID, SectionID, RoleID) VALUES "
-for loop in range(1, 6): #600 total teacher enrollments
-    for teacherUID in range(1, 100):
+
+for loop in range(5):  # 600 total teacher enrollments
+    for teacherUID in range(1, 101):
         sID = random.randrange(1, 200)
-        enrollment_sql += f"({teacherUID}, {sID}, {1}) "
-for loop in range(1, 11): #4400 total student enrollments
+        enrollment_sql += f"({teacherUID}, {sID}, 1), "
+        
+for loop in range(10):  # 4400 total student enrollments
     for studentUID in range(101, 500):
         sID = random.randrange(1, 200)
-        enrollment_sql += f"({studentUID}, {sID}, {2}) "
+        enrollment_sql += f"({studentUID}, {sID}, 2), "
+enrollment_sql = enrollment_sql.rstrip(', ') + ';'
 print(enrollment_sql)
-conn.execute(enrollment_sql)
+cursor.execute(enrollment_sql)
 
-finalgrade_sql = "INSERT INTO jph_finalgrade (EnrollmentID, GradeLetterID) VALUES "
+finalgrade_sql = "INSERT INTO jph_finalgrades (EnrollmentID, GradeLetterID) VALUES "
 for eID in range(101, 500):
     glID = random.randrange(1, 5)
-    finalgrade_sql += f"({eID}, {glID}) "
+    finalgrade_sql += f"({eID}, {glID}), "
+finalgrade_sql = finalgrade_sql.rstrip(', ') + ';'
 print(finalgrade_sql)
-conn.execute(finalgrade_sql)
+cursor.execute(finalgrade_sql)
 
-
-conn.commit()
-conn.close()
-
-data = conn.fetchall()
+connection.commit()
+cursor.close()
+connection.close()
